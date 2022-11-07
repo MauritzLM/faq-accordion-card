52fb3931c2deb5836c179583ee1416cef11cd769
Mauritz Marais <mauritz303@gmail.com>
2022-11-07 18:33:25 +0200

    added answer toggle with javascript, updated mobile styles

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // select all li's add click event
 // select all answers and toggle hidden class on click
 
-const questions = document.querySelectorAll('li');
+const questions = document.querySelectorAll('.question');
+const answers = document.querySelectorAll('.answer');
+const arrows = document.querySelectorAll('li img');
+
+function clickEvent() {
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', () => {
+            questions[i].classList.toggle('bold');
+            answers[i].classList.toggle('hidden');
+            arrows[i].classList.toggle('rotate')
+        })
+    }
+}
+
+clickEvent();
+
 
