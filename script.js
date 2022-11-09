994cee0bce281d61fe59c564d6ddd53d43f62e4f
Mauritz Marais <mauritz303@gmail.com>
2022-11-09 15:04:46 +0200

    updated desktop styles

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 // function to add click Event to all question blocks
-// the click event toggles css classes on the question(<p>), answer(<p>) and img elements 
+// the click event toggles css classes on the question(<p>), answer(<p>) and img elements (arrow)
 (function clickEvent() {
+
     const questionBlock = document.querySelectorAll('li');
     const questions = document.querySelectorAll('.question');
     const answers = document.querySelectorAll('.answer');
@@ -10,6 +11,7 @@
 
     for (let i = 0; i < questionBlock.length; i++) {
         questionBlock[i].addEventListener('click', () => {
+
             questions[i].classList.toggle('bold');
             answers[i].classList.toggle('hidden');
             arrows[i].classList.toggle('rotate');
